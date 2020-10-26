79958fb071bd3b06ac4a249135ecc362b23f4c8a
FelipeXR20 <felipexr20@live.com>
2020-10-26 16:05:07 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DetetiveLogin.Models;
+
+namespace DetetiveLogin.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DetetiveLogin.Models.TipoUsuario> TipoUsuario { get; set; }
+        public DbSet<DetetiveLogin.Models.AcessoTipoUsuario> AcessoTipoUsuario { get; set; }
+        public DbSet<DetetiveLogin.Models.PerfilUsuario> PerfilUsuario { get; set; }
+    }
+}
